1e03dfaa3fcd8cde7319ea3fcebb119d01980183
debd1987 <deb.d786@gmail.com>
2021-11-02 10:31:14 +0530

    Initial Commit

---
+++ src/main/java/com/ss/gw/SsGwServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ss.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsGwServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsGwServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/gw/SsGwServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsGwServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
